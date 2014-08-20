1b5adcc110674af43762a87246168cc8ab40c141
Scott Collis <scollis.acrf@gmail.com>
2014-08-20 16:51:39 -0500

    ADD:data directory to be filled using the vagrant script, also script for use outside of Vagrant

---
+++ data/get_data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wget https://engineering.arm.gov/~collis/osrsc/corrected_csapr.nc
+wget https://engineering.arm.gov/~collis/osrsc/corrected_xnwsapr.nc
+wget https://engineering.arm.gov/~collis/osrsc/corrected_xsesapr.nc
+wget https://engineering.arm.gov/~collis/osrsc/corrected_xswsapr.nc
+wget https://engineering.arm.gov/~jhelmus/sample_xsapr_a1_files/2013_12_03_NSA/nsaxsaprrhiC1.a1/nsaxsaprrhiC1.a1.20131203.141936.nc
+
+
