555fa81e327ca18ee1ee3878929d1a7a79e6d546
Scott Collis <scollis.acrf@gmail.com>
2014-08-21 10:22:21 -0500

    ADD: Three notebooks for the course. ADD: Updated the get_data.sh script

--- data/get_data.sh
+++ data/get_data.sh
@@ -5,5 +5,8 @@ wget https://engineering.arm.gov/~collis/osrsc/corrected_xnwsapr.nc
 wget https://engineering.arm.gov/~collis/osrsc/corrected_xsesapr.nc
 wget https://engineering.arm.gov/~collis/osrsc/corrected_xswsapr.nc
 wget https://engineering.arm.gov/~jhelmus/sample_xsapr_a1_files/2013_12_03_NSA/nsaxsaprrhiC1.a1/nsaxsaprrhiC1.a1.20131203.141936.nc
-
+wget http://engineering.arm.gov/~collis/KAMX_20140417_1056
+wget https://engineering.arm.gov/~collis/osrsc/KDVN_110608.nexrad
+wget https://engineering.arm.gov/~collis/osrsc/KILX_110437.nexrad
+wget https://engineering.arm.gov/~collis/osrsc/KLOT_110622.nexrad
 
