cd2aca3b96dab5d4d71a3ee5c8e3291e218879e8
Jonathan J. Helmus <jjhelmus@gmail.com>
2015-11-16 14:52:30 -0600

    Change URL of NSA XSAPR example file

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# The NSA XSAPR file is no longer available
-#wget --no-check-certificate https://engineering.arm.gov/~jhelmus/sample_xsapr_a1_files/2013_12_03_NSA/nsaxsaprrhiC1.a1/nsaxsaprrhiC1.a1.20131203.141936.nc
+wget --no-check-certificate https://engineering.arm.gov/~jhelmus/nsaxsaprrhiC1.a1.20131203.141936.nc
 wget --no-check-certificate https://engineering.arm.gov/~collis/KAMX_20140417_1056
 wget --no-check-certificate https://engineering.arm.gov/~collis/osrsc/KDVN_110608.nexrad
 wget --no-check-certificate  https://engineering.arm.gov/~collis/osrsc/KILX_110437.nexrad
