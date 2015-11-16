3ccf559a4d1117801f7ce0fa6a6eaec581a2eedc
Jonathan J. Helmus <jjhelmus@gmail.com>
2015-11-16 14:17:26 -0600

    Do not check https certificates
    Removes XSAPR file that is no longer available.

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-wget https://engineering.arm.gov/~jhelmus/sample_xsapr_a1_files/2013_12_03_NSA/nsaxsaprrhiC1.a1/nsaxsaprrhiC1.a1.20131203.141936.nc
-wget http://engineering.arm.gov/~collis/KAMX_20140417_1056
-wget https://engineering.arm.gov/~collis/osrsc/KDVN_110608.nexrad
-wget https://engineering.arm.gov/~collis/osrsc/KILX_110437.nexrad
-wget https://engineering.arm.gov/~collis/osrsc/KLOT_110622.nexrad
-wget https://engineering.arm.gov/~collis/osrsc/IL_grid.nc
+# The NSA XSAPR file is no longer available
+#wget --no-check-certificate https://engineering.arm.gov/~jhelmus/sample_xsapr_a1_files/2013_12_03_NSA/nsaxsaprrhiC1.a1/nsaxsaprrhiC1.a1.20131203.141936.nc
+wget --no-check-certificate https://engineering.arm.gov/~collis/KAMX_20140417_1056
+wget --no-check-certificate https://engineering.arm.gov/~collis/osrsc/KDVN_110608.nexrad
+wget --no-check-certificate  https://engineering.arm.gov/~collis/osrsc/KILX_110437.nexrad
+wget --no-check-certificate https://engineering.arm.gov/~collis/osrsc/KLOT_110622.nexrad
+wget --no-check-certificate https://engineering.arm.gov/~collis/osrsc/IL_grid.nc
 wget https://github.com/jjhelmus/pyart2baltrad/raw/master/data/sgpcsaprppi_20110520095101.nc
-wget https://engineering.arm.gov/~collis/osrsc/CHL20100621_222020
+wget --no-check-certificate https://engineering.arm.gov/~collis/osrsc/CHL20100621_222020
+
